54479e429fb381b18cac34ca603bf4500234d3ca
Tim Fletcher <tim@refsix.com>
2024-09-24 09:10:47 +0100

    Initial commit

---
+++ Patches/Patches.cs
@@ -0,0 +1,66 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+
+using UnityEngine;
+
+
+using HarmonyLib;
+
+
+namespace MyFirstSailwindPlugin.Patches
+{
+    internal class GPButtonSteeringWheelPatches
+    {
+        static GPButtonSteeringWheel wheel;
+        static GoPointer pointer;
+        
+        public static string GetGameObjectPath(GameObject obj)
+        {
+            string path = "/" + obj.name;
+            while (obj.transform.parent != null)
+            {
+                obj = obj.transform.parent.gameObject;
+                path = "/" + obj.name + path;
+            }
+            return path;
+        }
+
+        [HarmonyPatch(typeof(GPButtonSteeringWheel), "OnActivate")]
+        internal class GPButtonSteeringWheelOnActivate
+        {
+            public static void Postfix(GPButtonSteeringWheel __instance, GoPointer activatingPointer)
+            {
+                wheel = __instance;
+                pointer = activatingPointer;
+
+                var log = BepInEx.Logging.Logger.CreateLogSource("GPButtonSteeringWheel");
+                log.LogInfo($"{__instance.name} Awake");
+                log.LogInfo($"{GetGameObjectPath(__instance.gameObject)}");
+            }
+        }
+        
+        [HarmonyPatch(typeof(GoPointer), "LateUpdate")]
+        internal class GoPointerExtraLateUpdate
+        {
+            public static void Postfix()
+            {
+                var log = BepInEx.Logging.Logger.CreateLogSource("GPButtonSteeringWheel");
+                log.LogInfo("Update");
+                if(wheel == null)
+                    return;
+                if(pointer == null)
+                    return;
+                log.LogInfo($"{GetGameObjectPath(wheel.gameObject)}");
+                if (Input.GetKey(KeyCode.Alpha0))
+                {
+                    log.LogInfo("Key");
+
+                    wheel.OnActivate(pointer);
+                }
+            }
+        }
+        
+        
+    }
+
+}
---
+++ Plugin.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Reflection;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine;
+
+namespace MyFirstSailwindPlugin;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    internal static new ManualLogSource Logger;
+
+    private void Awake()
+    {
+        // Plugin startup logic
+        Logger = base.Logger;
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!!!");
+
+        Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), MyPluginInfo.PLUGIN_GUID);
+        
+    }
+
+}
